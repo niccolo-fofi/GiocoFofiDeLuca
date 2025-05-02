9299fd88f2fbb72a5da04af3a6bcfd4ed1e18bf2
niccolo-fofi <fofiniccolo@gmail.com>
2025-05-02 15:50:56 +0200

    Modificato il file primaMappa e aggiunti i primi segmenti di mappa

--- src/it/edu/iisgubbio/giocoFofiDeLuca/PrimaMappa.java
+++ src/it/edu/iisgubbio/giocoFofiDeLuca/PrimaMappa.java
@@ -1,13 +1,9 @@
 package it.edu.iisgubbio.giocoFofiDeLuca;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -18,9 +14,13 @@ import javafx.stage.Stage;
 public class PrimaMappa extends Application {
 
 	ImageView[][] mappa;
-	Image muro = new Image(getClass().getResourceAsStream("nero.jpg"));
-	Image vuoto = new Image(getClass().getResourceAsStream("bianco.jpg"));
+	Image muroVerticale = new Image(getClass().getResourceAsStream("verticale.png"));
+	Image muroOrizzontale = new Image(getClass().getResourceAsStream("orizzontale.png"));
 	Image puntino = new Image(getClass().getResourceAsStream("puntino.png"));
+	Image angoloDestro = new Image(getClass().getResourceAsStream("AngoloDestro.png"));
+	Image angoloSinistro= new Image(getClass().getResourceAsStream("AngoloSinistro.png"));
+	Image angoloT = new Image(getClass().getResourceAsStream("AngoloT.png"));
+
 	int altezzaMappa = 20;
 	int larghezzaMappa = 50;
 
@@ -40,17 +40,51 @@ public class PrimaMappa extends Application {
 		 */
 
 		try (
-				InputStream is = getClass().getResourceAsStream("posizioneMappa");
+				InputStream is = getClass().getResourceAsStream("posizioneMappa.txt");
 				InputStreamReader isr = new InputStreamReader(is);
 				BufferedReader lettore = new BufferedReader(isr);
 				) {
 
+			int y=0;
 			String rigaLetta;
 			while ((rigaLetta = lettore.readLine()) != null) {
-				String caratteri[] = rigaLetta.split(" ");
-				for(int i=0;i<caratteri.length;i++) {
-					System.out.print(caratteri +" ");
+
+				String caratteri[] = rigaLetta.split(",");
+
+				for (int x = 0; x < caratteri.length; x++) {
+					Image img=null;
+					if (caratteri[x].equals("v")) {
+					} else {
+						if (caratteri[x].equals("mV")) {
+							img = muroVerticale;
+						}else {
+							if (caratteri[x].equals("mO")) {
+								img = muroOrizzontale;
+							}else {
+								if (caratteri[x].equals("p")) {
+									img = puntino;
+								}else {
+									if (caratteri[x].equals("aD")) {
+										img = angoloDestro;
+									}else {
+										if(caratteri[x].equals("aT")) {
+											img = angoloT;
+										}
+									}
+								}
+							}
+						}
+					}
+
+					ImageView immagine = new ImageView(img);
+					immagine.setFitWidth(32);
+					immagine.setFitHeight(32);
+					immagine.setPreserveRatio(false);
+
+					mappa[x][y] = immagine;
+					g.add(immagine, x, y);
 				}
+				y++;
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
